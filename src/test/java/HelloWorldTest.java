c9f12437c5e5b1712f22c1aee9c9b747ed41e85b
CyberspaceSage <vidyaevda@gmail.com>
2025-11-18 22:44:19 +0300

    FirstAPITest

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,9 +1,17 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 public class HelloWorldTest {
     @Test
     public void testHelloWorld(){
-        System.out.println("Hello world");
+        Response response = RestAssured
+                .given()
+                .relaxedHTTPSValidation()  // добавляем для обхода SSL-проверки
+                .get("https://playground.learnqa.ru/api/hello")
+                .andReturn();
+        response.prettyPrint();
+
     }
 }
 
